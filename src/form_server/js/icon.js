6b34c1614b2ffc414ab14e5feefad189e8ba3a3f
Svante Lidman <svante.lidman@coreboost.se>
2016-01-27 17:40:22 +0100

    Added handler to icon

--- src/form_server/js/icon.js
+++ src/form_server/js/icon.js
@@ -2,8 +2,17 @@ const React = require('react');
 const renderWidgets = require('./renderwidgets.js');
 const Icon = React.createClass({
   componentWillMount: function () {
-    // Need to parse and hook
-    // on_key_handl
+    var this_ = this;
+    this.on_key_handlers = [];
+    this.props.definition.on_key_handlers.forEach(function (handler) {
+      var fn = eval(handler.behavior);
+      this_.on_key_handlers.push(
+        {
+          name: handler.name,
+          behavior: fn
+        }
+      );
+    });
   },
   render: function () {
     var icons  = [];
